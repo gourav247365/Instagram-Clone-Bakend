7e1089e528c14cba350eab7ae14e0e68fccdcf2a
Gourav <gouravnagar2020@gmail.com>
2025-01-14 16:05:19 +0530

    add minor change for testing

--- src/app.js
+++ src/app.js
@@ -1,57 +1,81 @@
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
-import {createServer} from 'http'
-import {Server} from 'socket.io'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
 
-const app= express()
-const server= createServer(app)
-const io= new Server(server, 
-  {cors: {origin: 'http://localhost:5173',methods: ["GET","POST"]}}
+const app = express()
+const server = createServer(app)
+const io = new Server(server,
+  { cors: { origin: process.env.SOCKET_CORS_ORIGIN, methods: ["GET", "POST"] } }
 )
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credential: true
-}))
-app.use(express.json({
-    limit: "16kb"
-}))
-app.use(express.urlencoded({
-    extended: true, limit: "16kb"
-}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(
+  cors(
+    {
+      origin: process.env.CORS_ORIGIN,
+      credential: true
+    }
+  )
+)
+app.use(
+  express.json(
+    {
+      limit: "16kb"
+    }
+  )
+)
+app.use(
+  express.urlencoded(
+    {
+      extended: true,
+      limit: "16kb"
+    }
+  )
+)
+app.use(
+  express.static("public")
+)
+app.use(
+  cookieParser()
+)
 
-server.listen(4001,()=> {
-  console.log("Socket Server listening on port 4001");
-  
+server.listen(4001, () => {
+  console.log("Socket Server listening on port 4001")
 })
 
-io.on('connection',(socket)=> {
-  console.log('A User Connected');
+io.on('connection', (socket) => {
 
-  socket.on('join',(chatId)=> {
+  console.log('A User Connected')
+
+  socket.on('join', (chatId) => {
     socket.join(chatId)
-    console.log('room Joined',chatId)
+    console.log('room Joined', chatId)
   })
-  
-  socket.on('message',(data)=> {    
+
+  socket.on('message', (data) => {
     socket.to(data.chatId).emit('getMessage', data)
   })
 
-  socket.on('typing',(data)=> {    
-    socket.to(data.chatId).emit('getTyping',data)
+  socket.on('typing', (data) => {
+    socket.to(data.chatId).emit('getTyping', data)
   })
 
-  socket.on('deleteMessage',(data)=> {
-    socket.to(data.chatId).emit('getDeleteMessage',(data))
+  socket.on('deleteMessage', (data) => {
+    socket.to(data.chatId).emit('getDeleteMessage', (data))
   })
 
 })
 
+// test route
+app.get('',(req,res)=> {
+  res.send("<h1>Hello World</h1>")
+})
+app.get('/test',(req,res)=> {
+  res.send("<h1>Success</h1>")
+})
 
-// //routes import
+// routes import
 import userRouter from './routes/user.route.js'
 import relationRouter from './routes/relation.routes.js'
 import postRouter from './routes/post.routes.js'
@@ -62,13 +86,14 @@ import messageRouter from './routes/message.route.js'
 import commentRouter from './routes/comment.route.js'
 
 // routes declaration
-app.use('/api/v1/users',userRouter)
-app.use('/api/v1/relations',relationRouter)
-app.use('/api/v1/posts',postRouter)
-app.use('/api/v1/likes',likeRouter)
-app.use('/api/v1/chats',chatRouter)
-app.use('/api/v1/notifications',notificationRouter)
-app.use('/api/v1/messages',messageRouter)
-app.use('/api/v1/comments',commentRouter)
+app.use('/api/v1/users', userRouter)
+app.use('/api/v1/relations', relationRouter)
+app.use('/api/v1/posts', postRouter)
+app.use('/api/v1/likes', likeRouter)
+app.use('/api/v1/chats', chatRouter)
+app.use('/api/v1/notifications', notificationRouter)
+app.use('/api/v1/messages', messageRouter)
+app.use('/api/v1/comments', commentRouter)
+
 
 export { app }
